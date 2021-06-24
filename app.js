7f24072c90a6570ed4697e3d07c6831a2f2d9ec9
ariel3259 <susanaarcia48@gmail.com>
2021-06-24 17:01:46 -0300

    rama terminada

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const { Console } = require('console');
-const mysql=require('mysql');
+const express=require('express');
+
+const app= express();
+
+
+app.get("/",(req,res)=>res.send(`
+<meta charset="UTF-8">
+<title>Primera vez con js</title>
+<link  href="style.css" rel="stylesheet" type="css/text">
+<h1>Caja 1</h1>
+`))
+app.listen("3000",err=>{
+if(err) throw err;
+console.log("Página funcional");
+});
+
+/*Como conectarse a mysql y  realizar peticiones
 const con=mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"",
     database:"mydb"
 });
+const mysql=require('mysql');
 con.connect(err=>{
     if(err)throw err;
     console.log("Conected");
-    var id=con.query("select id from users where name='A'",(err)=>{
+    var id=con.query("peticion sql",(err)=>{
         if(err)throw err;
-        console.log("consulta sql");
+        console.log("peticion cumplida");
     });
 
 })
-
+*/
